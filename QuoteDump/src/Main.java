6f82ffbd4e571a240e1c6f8c97cabcce4fd4b647
Justin T <37750742+jtieri@users.noreply.github.com>
2019-04-25 13:16:53 -0700

    finished
    Added the ability to write output to a file and also the ability to re-add quotes to a file that was already stripped.

--- QuoteDump/src/Main.java
+++ QuoteDump/src/Main.java
@@ -3,28 +3,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final String outputFile = "output\\output.txt";
+        final String outputFile = "output\\output.txt"; // default output file location/name
 
         Scanner keyboard = new Scanner(System.in);
+        Scanner inputFileReader;
+        File inputFile;
+        String inputFileName;
+        int functionalityOp;
 
         System.out.println("Enter the file whose contents you want to QuoteDump.");
 
-        String fileName = keyboard.nextLine();
-        File inputFile = new File(fileName);
+        inputFileName = keyboard.nextLine();
+        inputFile = new File(inputFileName);
 
-        Scanner inputFileReader = null;
         try {
             inputFileReader = new Scanner(inputFile);
         } catch (IOException ex) {
-            System.out.printf("The file %s was not found on the filesystem", inputFile);
+            throw new RuntimeException("The file " + inputFileName + "was not found on the filesystem");
         }
 
-        inputFileReader.useDelimiter("\""); // Scanner will read up till the first " instead of default whitespace
+        System.out.println("Enter 1 to strip quotes or 2 to reinsert quotes.");
 
-        while (inputFileReader.hasNext()) {
-            String nextString = inputFileReader.next();
+        functionalityOp = keyboard.nextInt();
 
-            System.out.println(nextString);
+        if (functionalityOp == 1) {
+            inputFileReader.useDelimiter("\""); // Scanner will read up till the first " instead of default whitespace
+
+            try (BufferedWriter outputFileWriter = new BufferedWriter(new FileWriter(outputFile))){
+                while (inputFileReader.hasNext()) {
+                    String nextString = inputFileReader.next();
+
+                    if (! (nextString.equals(" "))) {
+                        outputFileWriter.write(nextString + "\n");
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.printf("There was an error writing to the output file %s", outputFile);
+            }
+
+        } else if (functionalityOp == 2) {
+            try (BufferedWriter outputFileWriter = new BufferedWriter(new FileWriter(outputFile))){
+                while (inputFileReader.hasNext()) {
+                    String nextString = inputFileReader.next();
+
+                    if (! (nextString.equals(" "))) {
+                        outputFileWriter.write("\"" + nextString + "\"" + " ");
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.printf("There was an error writing to the output file %s", outputFile);
+            }
         }
 
         keyboard.close();
